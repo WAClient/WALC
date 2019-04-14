f515fb4a11122ff60e4ad3f4d2e44cb838da5207
Muhammad Tayyab Sheikh (CS Tayyab) <cstayyab@gmail.com>
2019-04-14 15:34:18 +0500

    Re-Fixed WhatsApp Chrome Version Error

--- main.js
+++ main.js
@@ -2,47 +2,39 @@ const { app, BrowserWindow, session } = require('electron')
 var path = require('path')
 
 // set user agent manually
-const userAgent = 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'
-
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const userAgent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.80 Safari/537.36'
+    // Keep a global reference of the window object, if you don't, the window will
+    // be closed automatically when the JavaScript object is garbage collected.
 let win
 
-function createWindow () {
-	// bypass error:
-	// WhatsApp works with Google Chrome 36+
-	// To use WhatsApp, update Chrome or use Mozilla Firefox, Safari, Microsoft Edge or Opera.
-	session.defaultSession.webRequest.onBeforeSendHeaders((details, callback) => {
-		details.requestHeaders['User-Agent'] = userAgent
-		callback({ cancel: false, requestHeaders: details.requestHeaders })
-	})
-	
-  // Create the browser window.
-  win = new BrowserWindow({ width: 800, height: 600, title: 'WhatsApp Linux (unofficial)', icon: path.join(__dirname, 'icons/logo256x256.png') })
+function createWindow() {
 
-  //Prevent windows title from changing
-  win.on('page-title-updated', (evt) => {
-    evt.preventDefault();
-  });
+    // Create the browser window.
+    win = new BrowserWindow({ width: 800, height: 600, title: 'WhatsApp Linux (unofficial)', icon: path.join(__dirname, 'icons/logo256x256.png') })
 
-  //Hide Default menubar
-  win.setMenu(null);
+    //Prevent windows title from changing
+    win.on('page-title-updated', (evt) => {
+        evt.preventDefault();
+    });
 
-  // and load the index.html of the app.
-  win.loadURL('https://web.whatsapp.com')
-  
-  win.setTitle('WhatsApp Linux (unofficial)')
+    //Hide Default menubar
+    win.setMenu(null);
 
-  // Emitted when the window is closed.
-  win.on('closed', () => {
-    // Dereference the window object, usually you would store windows
-    // in an array if your app supports multi windows, this is the time
-    // when you should delete the corresponding element.
-    win = null
-  })
+    // and load the index.html of the app.
+    win.loadURL('https://web.whatsapp.com', { 'userAgent': userAgent })
+
+    win.setTitle('WhatsApp Linux (unofficial)')
+
+    // Emitted when the window is closed.
+    win.on('closed', () => {
+        // Dereference the window object, usually you would store windows
+        // in an array if your app supports multi windows, this is the time
+        // when you should delete the corresponding element.
+        win = null
+    })
     win.webContents.on('new-window', (event, url) => {
         event.preventDefault()
-        require("electron").shell.openExternal(url);
+        app.shell.openExternal(url);
     })
 
 }
