e07a209a307af0b9d9fbb2a270d56dd249fc72a7
Daffa Mumtaz <daffamumtaz2001@gmail.com>
2021-05-05 01:02:05 +0700

    bug fixes: - update whatsap-web-electron.js - make sure whatsapp web is not loaded in dashboard window - unregister service worker when asked to update browser - handle disconnect event and reload whatsapp

--- src/Main/DashboardWindow.js
+++ src/Main/DashboardWindow.js
@@ -33,15 +33,11 @@ module.exports = class DashboardWindow extends BrowserWindow {
 		);
 		this.setMenuBarVisibility(false);
 
-		// added timeout to prevent whatsapp being loaded to this window instead of main
-		// hopefully no race condition bug later...
-		setTimeout(() => {
-			this.loadFile('public/index.html').then(() => {
-				// just for debugging
-				// this.show();
-				// this.webContents.openDevTools();
-			});
-		}, 2000);
+		this.loadFile('public/index.html').then(() => {
+			// just for debugging
+			// this.show();
+			// this.webContents.openDevTools();
+		});
 
 		this.isQuiting = false;
 		
