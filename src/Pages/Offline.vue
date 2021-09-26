fa5ee101c61ad7ef8ac938aacf04e1a25a24f8f0
Daffa Mumtaz <daffamumtaz2001@gmail.com>
2021-09-26 15:02:37 +0700

    improved online check in offline page

--- src/Pages/Offline.vue
+++ src/Pages/Offline.vue
@@ -7,8 +7,9 @@
           Cannot access WhatsApp Web Service. This page will reload as soon as you are back online.
         </v-card-text>
         <v-card-actions>
+          <span>{{ retryText }}</span>
           <v-spacer></v-spacer>
-          <v-btn @click="retry">Retry now</v-btn>
+          <v-btn dark color="green" :loading="checking" @click="onlineCheck">Retry now</v-btn>
         </v-card-actions>
       </v-card>
     </v-main>
@@ -16,15 +17,54 @@
 </template>
 
 <script>
+import isOnline from 'is-online';
+
 export default {
   created() {
-    let timeout;
-    window.addEventListener('online', () => {
-      timeout = setTimeout(this.retry, 5000)
-    });
-    window.addEventListener('offline', () => clearTimeout(timeout));
+    this.onlineCheck();
+  },
+  data() {
+    return {
+      timeouts: [3, 5, 10, 30, 60],
+      timeIdx: 0,
+      timeLeft: 0,
+      checking: false,
+    }
+  },
+  computed: {
+    /** @returns {string} */
+    retryText() {
+      if(this.timeLeft > 0) {
+        return `Retrying in ${this.timeLeft} seconds`;
+      } else if(this.checking) {
+        return 'Retrying...';
+      }
+      return '';
+    },
   },
   methods: {
+    async onlineCheck() {
+      this.checking = true;
+      const online = await isOnline();
+      if(online) {
+        this.retry();
+        return;
+      }
+
+      const timeout = this.timeouts[this.timeIdx];
+      setTimeout(() => this.onlineCheck(), timeout * 1000);
+      this.setTimeLeft(timeout);
+      if(this.timeIdx < this.timeouts.length - 1) {
+        this.timeIdx += 1;
+      }
+      this.checking = false;
+    },
+    setTimeLeft(number) {
+      this.timeLeft = number;
+      if(this.timeLeft > 0) {
+        setTimeout(() => this.setTimeLeft(number - 1), 1000);
+      }
+    },
     retry() {
       window.WALC.load();
     }
